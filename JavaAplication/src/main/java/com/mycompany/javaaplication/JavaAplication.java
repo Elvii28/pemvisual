ef1c33f737250e417eae35ea5a493fbc24e7f22f
Elvii28 <elviw31@gmail.com>
2024-08-07 14:51:03 +0700

    modul1-3

---
+++ JavaAplication/src/main/java/com/mycompany/javaaplication/JavaAplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaaplication;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class JavaAplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
